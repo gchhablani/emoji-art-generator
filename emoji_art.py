df05ab99ba044289b1d3e115136e9187a2bd50a0
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-03-31 06:49:35 +0530

    Allow larger emoji generation

--- emoji_art.py
+++ emoji_art.py
@@ -1,40 +1,143 @@
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
+import math
 
-if __name__ == '__main__':
+
+def is_emoji(s):
+    range_min = ord("\U0001F300")  # 127744
+    range_max = ord("\U0001FAD6")  # 129750
+    range_min_2 = 126980
+    range_max_2 = 127569
+    range_min_3 = 169
+    range_max_3 = 174
+    range_min_4 = 8205
+    range_max_4 = 12953
+
+    char_code = ord(s)
+    if range_min <= char_code <= range_max:
+        # or range_min_2 <= char_code <= range_max_2 or range_min_3 <= char_code <= range_max_3 or range_min_4 <= char_code <= range_max_4:
+        return True
+    elif range_min_2 <= char_code <= range_max_2:
+        return True
+    elif range_min_3 <= char_code <= range_max_3:
+        return True
+    elif range_min_4 <= char_code <= range_max_4:
+        return True
+    return False
+
+
+def generate_char(char, args):
+    img = cv2.imread(f"data/{char}/{args.font_style}.png")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if char.isupper():
+        img = cv2.resize(img, (args.height, args.width), interpolation=cv2.INTER_CUBIC)
+    elif is_emoji(char):
+        img = cv2.resize(
+            img,
+            (int(0.9 * args.height), int(0.9 * args.width)),
+            interpolation=cv2.INTER_CUBIC,
+        )
+
+        img = np.pad(
+            img,
+            ((args.height - img.shape[0], 0), (args.width - img.shape[1], 0)),
+            constant_values=((255, 255), (255, 255)),
+        )
+    else:
+        img = cv2.resize(
+            img,
+            (int(0.8 * args.height), int(0.8 * args.width)),
+            interpolation=cv2.INTER_CUBIC,
+        )
+
+        img = np.pad(
+            img,
+            ((args.height - img.shape[0], 0), (args.width - img.shape[1], 0)),
+            constant_values=((255, 255), (255, 255)),
+        )
+
+    ret2, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    if args.no_output:
+        plt.imshow(img)
+        plt.show()
+    return img
+
+
+# WhatsApp Space Equivalent: '      ' # Linux Space Equivalent: '  '
+
+
+if __name__ == "__main__":
 
     parser = ArgumentParser(
         prog="emoji_art.py", description="Generate an emoji art using text or image."
     )
     parser.add_argument(
-        "-mode", type=str, action="store", required=True, help="The mode for generation: either `image` or `text`."
+        "-mode",
+        type=str,
+        action="store",
+        required=True,
+        help="The mode for generation: either `image` or `text`.",
     )
     parser.add_argument(
-        "-input", type=str, action="store", required=True, help="The file path for `image` mode or the text for `text` mode. Text mode can also include emojis if available in generated data."
+        "-input",
+        type=str,
+        action="store",
+        required=True,
+        help="The file path for `image` mode or the text for `text` mode. Text mode can also include emojis if available in generated data.",
     )
     parser.add_argument(
-        "-height", type=int, action="store", default=300, help="The height of the image in 'character pixels' to be generated. In `text` mode this is for each character."
+        "-height",
+        type=int,
+        action="store",
+        default=300,
+        help="The height of the image in 'character pixels' to be generated. In `text` mode this is for each character.",
     )
     parser.add_argument(
-        "-width", type=int, action="store", default=None, required=False, help="The width of the image in 'character pixels' to be generated. If not provided, the height is equal to the width. In `text` mode this is for each character."
+        "-width",
+        type=int,
+        action="store",
+        default=None,
+        required=False,
+        help="The width of the image in 'character pixels' to be generated. If not provided, the height is equal to the width. In `text` mode this is for each character.",
     )
     parser.add_argument(
-        "-foreground_string", type=str, action="store", default='🖤', help="The foreground emoji/string for the art."
+        "-foreground_string",
+        type=str,
+        action="store",
+        default="🖤",
+        help="The foreground emoji/string for the art.",
     )
     parser.add_argument(
-        "-background_string", type=str, action="store", default='🤍', help="The background emoji/string for the art."
+        "-background_string",
+        type=str,
+        action="store",
+        default="🤍",
+        help="The background emoji/string for the art.",
     )
     parser.add_argument(
-        "-font_style", type=str, action="store", default="Monospace", help="The font-family to be used while generation. Note that with cursive fonts larger sizes are better."
+        "-font_style",
+        type=str,
+        action="store",
+        default="Monospace",
+        help="The font-family to be used while generation. Note that with cursive fonts larger sizes are better.",
     )
     parser.add_argument(
-        "--square_crop", action="store_true", help="Square crop the image in case the image is rectangular. Only used when mode is `image`. Cropping is done before generation."
+        "--square_crop",
+        action="store_true",
+        help="Square crop the image in case the image is rectangular. Only used when mode is `image`. Cropping is done before generation.",
     )
     parser.add_argument(
-        "--multiple_lines", action="store_true", help="Generate the text in multiple lines with each character in a new line. Only used when mode is `text`."
+        "--multiple_lines",
+        action="store_true",
+        help="Generate the text in multiple lines with each character in a new line. Only used when mode is `text`.",
+    )
+
+    parser.add_argument(
+        "--no_output",
+        action="store_true",
+        help="If the output is not to be printed, and only binarized image is to be shown. This is only useful for testing.",
     )
 
     args = parser.parse_args()
@@ -42,92 +145,87 @@ if __name__ == '__main__':
     if args.width is None:
         args.width = args.height
 
-    if args.mode == 'text':
+    if args.mode == "text":
         if args.multiple_lines:
             for char in args.input:
-                if char == ' ':
-                    print("\n"*4)
+
+                if char == " ":
+                    print("\n" * 4)
                     continue
-                img = cv2.imread(f'data/{char}/{args.font_style}.png')
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                if char.isupper():
-                    img = cv2.resize(
-                        img, (args.height, args.width), interpolation=cv2.INTER_CUBIC)
-                else:
-                    img = cv2.resize(
-                        img, (int(0.8*args.height), int(0.8*args.width)), interpolation=cv2.INTER_CUBIC)
-                output = ''
-                ret2, img = cv2.threshold(
-                    img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-                # WhatsApp: '      ' # Linux: '  '
-                new = np.where(img == 0, args.foreground_string,
-                               args.background_string)
-                for row in new:
-                    output += ''.join(row)+'\n'
-                print(output)
+                img = generate_char(char, args)
+
+                new = np.where(img == 0, args.foreground_string, args.background_string)
+
+                if not args.no_output:
+                    output = ""
+                    for row in new:
+                        output += "".join(row) + "\n"
+                    print(output)
         else:
             output_arr = None
             for char in args.input:
-                if char == ' ':
+                if char == " ":
                     if output_arr is None:
                         output_arr = np.array(
-                            [255, ]*(args.width)*args.height).reshape(args.height, -1)
+                            [
+                                255,
+                            ]
+                            * (args.width)
+                            * args.height
+                        ).reshape(args.height, -1)
                     else:
-                        output_arr = np.concatenate((output_arr, np.array(
-                            [255, ]*(args.width)*args.height).reshape(args.height, -1)), axis=1)
+                        output_arr = np.concatenate(
+                            (
+                                output_arr,
+                                np.array(
+                                    [
+                                        255,
+                                    ]
+                                    * (args.width)
+                                    * args.height
+                                ).reshape(args.height, -1),
+                            ),
+                            axis=1,
+                        )
                     continue
 
-                img = cv2.imread(f'data/{char}/{args.font_style}.png')
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                # print(char)
-                # print(args.font_style)
+                img = generate_char(char, args)
 
-                if char.isupper():
-                    img = cv2.resize(
-                        img, (args.height, args.width), interpolation=cv2.INTER_CUBIC)
-                else:
-                    img = cv2.resize(
-                        img, (int(0.8*args.height), int(0.8*args.width)), interpolation=cv2.INTER_CUBIC)
-                    # print(img.shape)
-                    img = np.pad(
-                        img, ((args.height-img.shape[0], 0), (args.width-img.shape[1], 0)), constant_values=((255, 255), (255, 255)))
-
-                ret2, img = cv2.threshold(
-                    img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
                 if output_arr is None:
                     output_arr = img
                 else:
                     output_arr = np.concatenate((output_arr, img), axis=1)
-            output = ''
             # WhatsApp: '      ' # Linux: '  '
-            new = np.where(output_arr == 0, args.foreground_string,
-                           args.background_string)
-            for row in new:
-                output += ''.join(row)+'\n'
-            print(output)
+            new = np.where(
+                output_arr == 0, args.foreground_string, args.background_string
+            )
+            if not args.no_output:
+                output = ""
+                for row in new:
+                    output += "".join(row) + "\n"
+                print(output)
 
-    elif args.mode == 'image':
+    elif args.mode == "image":
         img = cv2.imread(args.input)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         if args.square_crop:
             w, h = img.shape
             if w > h:
-                img = img[w//2-h//2:w//2+h//2, :]
+                img = img[w // 2 - h // 2 : w // 2 + h // 2, :]
             else:
-                img = img[:, h//2-w//2:h//2+w//2]
+                img = img[:, h // 2 - w // 2 : h // 2 + w // 2]
 
-        img = cv2.resize(img, (args.width, args.height),
-                         interpolation=cv2.INTER_CUBIC)
-        ret2, img = cv2.threshold(
-            img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        new = np.where(img == 0, args.foreground_string,
-                       args.background_string)
-        output = ''
-        for row in new:
-            output += ''.join(row)+'\n'
-        print(output)
+        img = cv2.resize(img, (args.width, args.height), interpolation=cv2.INTER_CUBIC)
+        ret2, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        new = np.where(img == 0, args.foreground_string, args.background_string)
+        if not args.no_output:
+            output = ""
+            for row in new:
+                output += "".join(row) + "\n"
+            print(output)
 
     else:
         raise NotImplementedError(
-            "Currently only `image` and `text` mode are supported.")
+            "Currently only `image` and `text` mode are supported."
+        )
--- generation/generate.py
+++ generation/generate.py
@@ -27,21 +27,26 @@ def save(path):
     h = np.argmin(a, axis=0)
     h[h == 0] = 5000
     right = min(h)
-    right = a.shape[0]-right
+    right = a.shape[0] - right
     # print(right)
     a = np.rot90(a)
     w = np.argmin(a, axis=0)
     w[w == 0] = 5000
     bottom = min(w)
-    bottom = a.shape[0]-bottom
+    bottom = a.shape[0] - bottom
     # print(bottom)
 
     a = cv2.imread(path, 0)
-    a = np.pad(a[top-1:bottom+1, left-1:right+1],
-               (1, 1), 'constant', constant_values=(255))
+    a = np.pad(
+        a[top - 1 : bottom + 1, left - 1 : right + 1],
+        (1, 1),
+        "constant",
+        constant_values=(255),
+    )
 
     cv2.imwrite(path, a)
     return a
+
 
 # main driver function to render html and take a screenshot
 
@@ -52,42 +57,46 @@ def generate_images():
     font_face = ""
     for i, j in enumerate(font_list):
         a = "@font-face { font-family: %s; src: url('./fonts/%s')} " % (
-            "'"+str(i)+"'", j)
-        font_face = font_face+a
+            "'" + str(i) + "'",
+            j,
+        )
+        font_face = font_face + a
     start_html = "<html> <head> <style>"
     end_html = "</font></p> </body> </html>"
-    mid_html = "</style> </head> <body> <p><font size="+str(15)+">"
+    mid_html = "</style> </head> <body> <p><font size=" + str(15) + ">"
 
     ranges = []
     ranges = list(range(65, 91))
     ranges += list(range(97, 123))
-    extra_list = ['💩', '👻', '❤', '🤗','.']  # add your characters here
+    extra_list = ["💩", "👻", "❤", "🤗", "."]  # add your characters here
     ranges += list(map(ord, extra_list))
 
     for i in ranges:
-        if not os.path.exists(os.path.join(current_path, "../data/"+chr(i))):
-            os.makedirs(os.path.join(current_path, "../data/"+chr(i)))
+        if not os.path.exists(os.path.join(current_path, "../data/" + chr(i))):
+            os.makedirs(os.path.join(current_path, "../data/" + chr(i)))
         # images = []
 
         for j in range(len(font_list)):
             img_name = os.path.join(
-                current_path, "../data/"+chr(i)+"/"+font_list[j].split('.')[0]+".png")
+                current_path,
+                "../data/" + chr(i) + "/" + font_list[j].split(".")[0] + ".png",
+            )
             if not os.path.exists(img_name):
                 print(chr(i))
                 f = open("generate.html", "w")
-                select_font = "* {font-family: %s}" % ("'"+str(j)+"'")
-                text = start_html+font_face + \
-                    select_font+mid_html+chr(i)+end_html
+                select_font = "* {font-family: %s}" % ("'" + str(j) + "'")
+                text = (
+                    start_html + font_face + select_font + mid_html + chr(i) + end_html
+                )
                 f.write(text)
                 f.close()
-                driver = webdriver.Chrome(os.path.join(current_path,'chromedriver'))
-                driver.get(
-                    "file://"+os.path.join(current_path, 'generate.html'))
+                driver = webdriver.Chrome(os.path.join(current_path, "chromedriver"))
+                driver.get("file://" + os.path.join(current_path, "generate.html"))
                 driver.save_screenshot(img_name)
                 img = save(img_name)
                 driver.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # text in the generate.html file
     generate_images()
